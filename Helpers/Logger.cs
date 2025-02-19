1c7a732b729b556d53f017e631b405f04dc49fe5
Sebastian Bigatton <sebastianbigatton@gmail.com>
2025-02-19 14:29:32 -0300

    add logger function

---
+++ Helpers/Logger.cs
@@ -0,0 +1,19 @@
+public class Logger
+{
+    public static void LogMessage(string filePath, string message, string logType)
+    {
+        var logMessage = $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] [{logType}] {message}";
+
+        try
+        {        
+            using var writer = File.AppendText(filePath);
+            lock(writer) {
+                writer.WriteLine(logMessage);
+            }   
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Logging into message into file failed: {ex.Message}");
+        }
+    }
+}
